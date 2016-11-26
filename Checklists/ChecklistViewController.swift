0d683c137478ca94bb955d44835f154b47f6e340
Oussama Bouguerne <oussama.bouguerne@metaworks.xyz>
2016-11-26 06:48:11 +0100

    Implement the TableView's data source and delegate (Selecting a row) methods

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UITableViewController {
+class ChecklistViewController: UITableViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -22,7 +22,59 @@ class ViewController: UITableViewController {
     let cell = tableView.dequeueReusableCell(
                           withIdentifier: "ChecklistItem", for: indexPath)
     
+    let label = cell.viewWithTag(1000) as! UILabel
+    
+    if indexPath.row == 0 {
+      label.text = "Walk the dog"
+    } else if indexPath.row == 1 {
+      label.text = "Brush my teeth"
+    } else if indexPath.row == 2 {
+      label.text = "Learn iOS development"
+    } else if indexPath.row == 3 {
+      label.text = "Soccer practice"
+    } else if indexPath.row == 4 {
+      label.text = "Eat ice cream"
+    }
+    
     return cell
   }
+  
+  override func tableView(_ tableView: UITableView,
+                          didSelectRowAt indexPath: IndexPath) {
+    
+    if let cell = tableView.cellForRow(at: indexPath) {
+      if cell.accessoryType == .none {
+        cell.accessoryType = .checkmark
+      } else {
+        cell.accessoryType = .none
+      }
+    }
+    
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
