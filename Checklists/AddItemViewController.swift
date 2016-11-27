fa094b2e45e5ae825b82a796aa0b0d83d33f05ba
Oussama Bouguerne <oussama.bouguerne@metaworks.xyz>
2016-11-27 01:42:26 +0100

    Further customize the design of the Add Item screen to better the app's UX

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -1,17 +1,39 @@
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+  
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var doneBarButton: UIBarButtonItem!
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    textField.becomeFirstResponder()
+  }
   
   @IBAction func cancel() {
     dismiss(animated: true, completion: nil)
   }
   
   @IBAction func done() {
+    print("Contents of the text field: \(textField.text!)")
+    
     dismiss(animated: true, completion: nil)
   }
   
   override func tableView(_ tableView: UITableView,
                           willSelectRowAt indexPath: IndexPath) -> IndexPath? {
     return nil
+  }
+  
+  func textField(_ textField: UITextField,
+                 shouldChangeCharactersIn range: NSRange,
+                 replacementString string: String) -> Bool {
+    
+    let oldText = textField.text! as NSString
+    let newText = oldText.replacingCharacters(in: range, with: string)
+                                              as NSString
+    doneBarButton.isEnabled = (newText.length > 0)
+    
+    return true
   }
 }
